42a265c7b0c3428da2b5a065df9a8b29deb1899c
czelabueno <c.zelabueno@gmail.com>
2024-08-06 01:16:58 -0400

    adding IT for new mistral models

--- langchain4j-mistral-ai/src/test/java/dev/langchain4j/model/mistralai/MistralAiCompletionModelIT.java
+++ langchain4j-mistral-ai/src/test/java/dev/langchain4j/model/mistralai/MistralAiCompletionModelIT.java
@@ -5,6 +5,8 @@ import dev.langchain4j.model.output.Response;
 import dev.langchain4j.model.output.TokenUsage;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static dev.langchain4j.model.output.FinishReason.STOP;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,8 +21,12 @@ class MistralAiCompletionModelIT {
         // When
         Response<String> response = codestral.generate(codePrompt);
         // Then
-        System.out.println(codePrompt +
-                response.content()); // print code completion
+        System.out.println(
+                String.format(
+                        "%s%s",
+                        codePrompt,
+                        response.content())
+                ); // print code completion
 
         TokenUsage tokenUsage = response.tokenUsage();
         assertThat(tokenUsage.inputTokenCount()).isGreaterThan(0);
@@ -38,19 +44,88 @@ class MistralAiCompletionModelIT {
                 .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
                 .modelName(MistralAiCodeModelName.CODESTRAL_LATEST)
                 .logRequests(true)
-                .logResponses(true)
                 .build();
 
-        String codePrompt = "public static void main(String[] args) {";
-        String suffix = "int n = scanner.nextInt();\nSystem.out.println(fibonacci(n));";
+        String codePrompt = "public class HelloWorld {\n" +
+                            "\tpublic static void main(String[] args) {\n" +
+                            "\t\tChatLanguageModel model = MistralAiChatModel.withApiKey(ApiKeys.MISTRALAI_API_KEY);";
+        String suffix = "\t\tSystem.out.println(response);\n" +
+                        "\t}\n" +
+                        "}";
 
         // When
         Response<String> response = codestral.generate(codePrompt, suffix);
         // Then
         System.out.println(
-                codePrompt +
-                response.content() +
-                suffix); // print code completion
+                String.format(
+                        "%s%s%s",
+                        codePrompt,
+                        response.content(),
+                        suffix)); // print code completion
+
+        assertThat(response.content()).doesNotContainIgnoringCase(codePrompt);
+        assertThat(response.content()).doesNotContainIgnoringCase(suffix);
+        assertThat(response.finishReason()).isEqualTo(STOP);
     }
 
+    @Test
+    void should_generate_code_completion_with_suffix_and_stops() {
+        // Given
+        MistralAiCompletionModel codestral = MistralAiCompletionModel.builder()
+                .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
+                .modelName(MistralAiCodeModelName.CODESTRAL_LATEST)
+                .stops(Arrays.asList("\n\n"))
+                .logRequests(true)
+                .build();
+
+        String codePrompt = "def is_odd(n): \n return n % 2 == 1 \n def test_is_odd():";
+        String suffix = "test_is_odd()";
+
+        // When
+        Response<String> response = codestral.generate(codePrompt, suffix);
+        // Then
+        System.out.println(
+                String.format(
+                        "%s%s",
+                        codePrompt,
+                        response.content())
+                ); // print code completion
+
+        assertThat(response.content()).doesNotContainIgnoringCase(codePrompt);
+        assertThat(response.finishReason()).isEqualTo(STOP);
+    }
+
+    @Test
+    void should_generate_code_completion_with_suffix_and_max_min_tokens() {
+        // Given
+        MistralAiCompletionModel codestral = MistralAiCompletionModel.builder()
+                .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
+                .modelName(MistralAiCodeModelName.CODESTRAL_LATEST)
+                .maxTokens(1024)
+                .minTokens(0)
+                .logRequests(true)
+                .build();
+
+        String codePrompt = "public class HelloWorld {\n" +
+                "\tpublic static void main(String[] args) {\n" +
+                "\t\tChatLanguageModel model = MistralAiChatModel.withApiKey(ApiKeys.MISTRALAI_API_KEY);";
+        String suffix = "\t\tSystem.out.println(response);\n" +
+                "\t}\n" +
+                "}";
+
+        // When
+        Response<String> response = codestral.generate(codePrompt, suffix);
+        // Then
+        System.out.println(
+                String.format(
+                        "%s%s%s",
+                        codePrompt,
+                        response.content(),
+                        suffix)
+        ); // print code completion
+
+        assertThat(response.content()).doesNotContainIgnoringCase(codePrompt);
+        assertThat(response.content()).doesNotContainIgnoringCase(suffix);
+        assertThat(response.finishReason()).isEqualTo(STOP);
+    }
 }
--- langchain4j-mistral-ai/src/test/java/dev/langchain4j/model/mistralai/MistralAiStreamingCompletionModelIT.java
+++ langchain4j-mistral-ai/src/test/java/dev/langchain4j/model/mistralai/MistralAiStreamingCompletionModelIT.java
@@ -6,6 +6,8 @@ import dev.langchain4j.model.output.Response;
 import dev.langchain4j.model.output.TokenUsage;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static dev.langchain4j.model.output.FinishReason.STOP;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,8 +27,12 @@ class MistralAiStreamingCompletionModelIT {
         Response<String> response = handler.get();
 
         // Then
-        System.out.println(codePrompt +
-                response.content()); // print code completion
+        System.out.println(
+                String.format(
+                        "%s%s",
+                        codePrompt,
+                        response.content())
+                ); // print code completion
 
         TokenUsage tokenUsage = response.tokenUsage();
         assertThat(tokenUsage.inputTokenCount()).isGreaterThan(0);
@@ -38,7 +44,7 @@ class MistralAiStreamingCompletionModelIT {
     }
 
     @Test
-    void should_generate_code_stream_completion_with_suffix() {
+    void should_stream_generate_code_completion_with_suffix() {
         // Given
         MistralAiStreamingCompletionModel codestral = MistralAiStreamingCompletionModel.builder()
                 .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
@@ -46,8 +52,12 @@ class MistralAiStreamingCompletionModelIT {
                 .logRequests(true)
                 .build();
 
-        String codePrompt = "public static void main(String[] args) {";
-        String suffix = "int n = scanner.nextInt();\nSystem.out.println(fibonacci(n));";
+        String codePrompt = "public class HelloWorld {\n" +
+                "\tpublic static void main(String[] args) {\n" +
+                "\t\tChatLanguageModel model = MistralAiChatModel.withApiKey(ApiKeys.MISTRALAI_API_KEY);";
+        String suffix = "\t\tSystem.out.println(response);\n" +
+                "\t}\n" +
+                "}";
 
         // When
         TestStreamingResponseHandler<String> handler = new TestStreamingResponseHandler<>();
@@ -57,8 +67,83 @@ class MistralAiStreamingCompletionModelIT {
 
         // Then
         System.out.println(
-                codePrompt +
-                response.content() +
-                suffix); // print code completion
+                String.format(
+                        "%s%s%s",
+                        codePrompt,
+                        response.content(),
+                        suffix)); // print code completion
+
+        assertThat(response.content()).doesNotContainIgnoringCase(codePrompt);
+        assertThat(response.content()).doesNotContainIgnoringCase(suffix);
+        assertThat(response.finishReason()).isEqualTo(STOP);
+    }
+
+    @Test
+    void should_stream_generate_code_completion_with_suffix_and_stops() {
+        // Given
+        MistralAiStreamingCompletionModel codestral = MistralAiStreamingCompletionModel.builder()
+                .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
+                .modelName(MistralAiCodeModelName.CODESTRAL_LATEST)
+                .stops(Arrays.asList("\n\n"))
+                .logRequests(true)
+                .build();
+
+        String codePrompt = "def is_odd(n): \n return n % 2 == 1 \n def test_is_odd():";
+        String suffix = "test_is_odd()";
+
+        // When
+        TestStreamingResponseHandler<String> handler = new TestStreamingResponseHandler<>();
+        codestral.generate(codePrompt, suffix, handler);
+
+        Response<String> response = handler.get();
+
+        // Then
+        System.out.println(
+                String.format(
+                        "%s%s",
+                        codePrompt,
+                        response.content())
+                ); // print code completion
+
+        assertThat(response.content()).doesNotContainIgnoringCase(codePrompt);
+        assertThat(response.finishReason()).isEqualTo(STOP);
+    }
+
+    @Test
+    void should_stream_generate_code_completion_with_suffix_and_max_min_tokens() {
+        // Given
+        MistralAiStreamingCompletionModel codestral = MistralAiStreamingCompletionModel.builder()
+                .apiKey(System.getenv("MISTRAL_AI_API_KEY"))
+                .modelName(MistralAiCodeModelName.CODESTRAL_LATEST)
+                .maxTokens(1024)
+                .minTokens(0)
+                .logRequests(true)
+                .build();
+
+        String codePrompt = "public class HelloWorld {\n" +
+                "\tpublic static void main(String[] args) {\n" +
+                "\t\tChatLanguageModel model = MistralAiChatModel.withApiKey(ApiKeys.MISTRALAI_API_KEY);";
+        String suffix = "\t\tSystem.out.println(response);\n" +
+                "\t}\n" +
+                "}";
+
+        // When
+        TestStreamingResponseHandler<String> handler = new TestStreamingResponseHandler<>();
+        codestral.generate(codePrompt, suffix, handler);
+
+        Response<String> response = handler.get();
+
+        // Then
+        System.out.println(
+                String.format(
+                        "%s%s%s",
+                        codePrompt,
+                        response.content(),
+                        suffix)
+        ); // print code completion
+
+        assertThat(response.content()).doesNotContainIgnoringCase(codePrompt);
+        assertThat(response.content()).doesNotContainIgnoringCase(suffix);
+        assertThat(response.finishReason()).isEqualTo(STOP);
     }
 }
